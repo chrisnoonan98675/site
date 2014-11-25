a7d8819120c1d93d402df2b7ba09354cc9a05fe2
Ivica Loncar <iloncar@monobit.co>
2014-11-25 11:49:36 +0100

    Added _fixperms.sh, removed execute bit from some files.

--- _sync.sh
+++ _sync.sh
@@ -27,28 +27,36 @@ if [ -z $XLDOC_HOME ]; then
 fi
 
 # XLD
+mkdir -p "$XLDOC_HOME/xl-deploy/3.9.x"
+mkdir -p "$XLDOC_HOME/xl-deploy/4.0.x"
+mkdir -p "$XLDOC_HOME/xl-deploy/4.5.x"
 rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/3.9/deployit/ $XLDOC_HOME/xl-deploy/3.9.x/
 rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.0/deployit/ $XLDOC_HOME/xl-deploy/4.0.x/
 rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.5/xl-deploy/ $XLDOC_HOME/xl-deploy/4.5.x/
 
 # XLD plugins
+mkdir -p "$XLDOC_HOME/xl-deploy/4.0.x/plugins"
 for i in bamboo-xl-deploy-plugin bigip-plugin biztalk-plugin glassfish-plugin iis-plugin jbossas-plugin jbossdm-plugin netscaler-plugin osb-plugin tfs-plugin tomcat-plugin was-plugin was-plugin-extensions windows-plugin wls-plugin wmq-plugin wp-plugin wps-plugin; do 
 	rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.0/${i} $XLDOC_HOME/xl-deploy/4.0.x/plugins
 done
 
+mkdir -p "$XLDOC_HOME/xl-deploy/3.9.x/plugins"
 for i in bamboo-xl-deploy-plugin bigip-plugin biztalk-plugin cloud-plugin ec2-plugin glassfish-plugin iis-plugin jbossas-plugin jbossdm-plugin netscaler-plugin osb-plugin tfs-plugin tomcat-plugini vsphere-plugin was-plugin was-plugin-extensions windows-plugin wls-plugin wmq-plugin wp-plugin wps-plugin; do 
 	rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/3.9/${i} $XLDOC_HOME/xl-deploy/3.9.x/plugins
 done
 
+mkdir -p "$XLDOC_HOME/xl-deploy/4.5.x/plugins"
 for i in bamboo-xl-deploy-plugin bigip-plugin jbossdm-plugin netscaler-plugin osb-plugin tfs-plugin tomcat-plugin was-plugin was-plugin-extensions wls-plugin wmq-plugin wps-plugin; do 
 	rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.5/${i} $XLDOC_HOME/xl-deploy/4.5.x/plugins
 done
 
 # XLR
+mkdir -p "$XLDOC_HOME/xl-release/4.0.x/"
 rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.0/xl-release/ $XLDOC_HOME/xl-release/4.0.x/
 rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.0/xl-release-server/rest-api $XLDOC_HOME/xl-release/4.0.x/
 
 # XLR plugins
+mkdir -p "$XLDOC_HOME/xl-release/4.0.x/plugins/"
 rsync -razv --chmod=u=rwX tech@tech.xebialabs.com:/home/tech/www/xl-online-docs/releases/4.0/xlr-*-plugin $XLDOC_HOME/xl-release/4.0.x/plugins/
 
 # XL scale
