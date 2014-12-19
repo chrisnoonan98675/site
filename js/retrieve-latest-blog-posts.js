392f2660880d7e77424e379d9d02b384c953a286
Amy Johnston <ajohnston@xebialabs.com>
2014-12-19 17:14:37 +0100

    DEPL-7698 Added JS to get latest topics from announcements forum

--- js/retrieve-latest-blog-posts.js
+++ js/retrieve-latest-blog-posts.js
@@ -3,13 +3,13 @@ var feedContainer=document.getElementById("feedContainer");
 
 $(document).ready(function() {
 	var feed = new google.feeds.Feed(feedUrl);
-	feed.setNumEntries(10);
+	feed.setNumEntries(5);
 	feed.load( function(result) {
-		list = "<ul>";
+		list = "<ul class='list-unstyled blogpostlist'>";
 		if (!result.error){
 			var posts=result.feed.entries;
 			for (var i = 0; i < posts.length; i++) { 
-				list+="<li><a href='" + posts[i].link + "'target='_blank' title='Posted " + posts[i].publishedDate + "'>" + posts[i].title + "</a></li>";
+				list+="<li><a href='" + posts[i].link + "'target='_blank' title='Posted " + posts[i].publishedDate + "'>" + posts[i].title + "</a><br /><small>" + posts[i].contentSnippet + "</small></li>";
 			}
 			list+="</ul>";
 			feedContainer.innerHTML = list;
