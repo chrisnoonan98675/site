85482dc8c0955aa0f3ec8e3af8c4036a4448b42d
Arjan Molenaar <gaphor@gmail.com>
2015-09-09 15:45:20 +0200

    Speed up breadcrumbs generation by creating a url to page cache.

--- _plugins/breadcrumbs.rb
+++ _plugins/breadcrumbs.rb
@@ -4,12 +4,20 @@ module Jekyll
         # Monkey patch Jekyll's Page class
         # Source: http://biosphere.cc/software-engineering/jekyll-breadcrumbs-navigation-plugin/
         class Page
+                @@url_to_page = {}
  
                 ##
                 # We add a custom method to the page variable, that returns an ordered list of it's
                 # parent pages ready for iteration.
                 def ancestors
                         a = []
+                        # Lazily create a cache of url to page mappings. This enhances the lookup speed.
+                        if @@url_to_page.empty?
+                                site.pages.map do |page|
+                                        @@url_to_page[page.url] = page
+                                end
+                        end
+
                         url = self.url
                         while url != "/index.html"
                                 pt = url.split("/")
@@ -21,7 +29,7 @@ module Jekyll
                                         # one level up
                                         url = pt[0..-3].join("/") + "/index.html"
                                 end
-                                a << get_page_from_url(url)
+                                a << @@url_to_page[url]
                         end
  
                         return a.reverse
@@ -34,16 +42,6 @@ module Jekyll
                         h = orig_to_liquid
                         h['ancestors'] = self.ancestors
                         return h
-                end
- 
-                private
- 
-                ##
-                # Gets Page object that has given url. Very efficient O(n) solution.
-                def get_page_from_url(url)
-                        site.pages.each do |page|
-                                return page if page.url == url
-                        end
                 end
         end
 end
