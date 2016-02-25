2c771c3dfbfd8383b624f0bbbb5050554696eb68
Bruno Devic <bdevic.dm@gmail.com>
2016-02-25 10:42:16 +0100

    Change dev dashboard url to tracker

--- _plugins/jira_dashboard.rb
+++ _plugins/jira_dashboard.rb
@@ -238,7 +238,7 @@ module Jekyll
       @site = site
       @base = base
       @dir = dir
-      @name = "#{product.name}-dashboard.html"
+      @name = "#{product.name}-tracker.html"
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'release_dashboard.html')
       self.data['product'] = product
